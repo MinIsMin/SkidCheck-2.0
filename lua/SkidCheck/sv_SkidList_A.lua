7f6475bc96217f2ab6aff1b4bf0227063c9f0d03
HeX <MFSiNC@users.noreply.github.com>
2015-09-24 21:24:24 +0100

    Added "Smile"

--- lua/SkidCheck/sv_SkidList_A.lua
+++ lua/SkidCheck/sv_SkidList_A.lua
@@ -17,6 +17,7 @@ local Skiddies = {
 
 
 
+	["STEAM_0:1:73281504"] = "Smile/MoonBass/Simon, Hera, anxition, lua/defiance.lua",
 	["STEAM_0:0:72423494"] = "OptiJack/Jack, Mapex, Lennys scripts",
 	["STEAM_0:0:126153968"] = "luis7882/luis, Member of snixzz hacking site",
 	["STEAM_0:0:84408790"] = "sp00k/The Terminator/Troll names, 4x VAC bans, Member of snixzz hacking site, Member of hack/troll group: myg0t_international",
