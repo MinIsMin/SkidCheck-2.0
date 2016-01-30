2e5575b8642da051c5365c09eb9ff9cbe503545d
HeX <MFSiNC@users.noreply.github.com>
2016-01-30 18:08:31 +0000

    Added "buckballs"

--- lua/SkidCheck/sv_SkidList_A.lua
+++ lua/SkidCheck/sv_SkidList_A.lua
@@ -16,7 +16,8 @@ local Skiddies = {
 
 
 
-
+	["STEAM_0:0:79310418"] = "buckballs, VAC banned, speedhacks",
+	
 	["STEAM_0:1:116271858"] = "DEXTRE/ScorpionTheDead/SongMan, sv_allowcslua bypass, defcon aimbot, gmcl_cat_win32.dll, Ban Evasion (Alt of STEAM_0:0:143814585)",
 	["STEAM_0:0:143814585"] = "DEXTRE/ScorpionTheDead/SongMan, sv_allowcslua bypass, defcon aimbot, gmcl_cat_win32.dll, Ban Evasion (STEAM_0:1:116271858)",
 	
