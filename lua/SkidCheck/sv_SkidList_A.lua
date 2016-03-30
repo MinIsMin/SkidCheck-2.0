7d719b89f98686920e4ad926273b0a88b4c7a5d9
HeX <MFSiNC@users.noreply.github.com>
2016-03-30 13:48:03 +0100

    Added more

--- lua/SkidCheck/sv_SkidList_A.lua
+++ lua/SkidCheck/sv_SkidList_A.lua
@@ -18,6 +18,7 @@ local Skiddies = {
 
 
 
+	["STEAM_0:0:54052778"] = "Kolorek/doswave, STEALING FILES, 5x VAC Bans, Cheat engine bypass, bridgehack, Lennys, Hera",
 	["STEAM_0:0:52543705"] = "Ale711, Member of hack/troll group: MPGH",
 	["STEAM_0:1:57234919"] = "Benjamin/skywastaken/Sky, VAC banned, Member of snixzz hacking site",
 	["STEAM_0:1:125356872"] = "Ger Sean/jumpz2105/Sean, STEALING FILES, Bridgehack, defcon aimbot, net exploits",
