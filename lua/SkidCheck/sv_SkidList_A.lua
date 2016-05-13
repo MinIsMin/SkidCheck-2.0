6f1def5696396f772c293aab247bb48c83ad6cd1
HeX <MFSiNC@users.noreply.github.com>
2016-05-13 14:22:05 +0100

    Added "Reaper" + more

--- lua/SkidCheck/sv_SkidList_A.lua
+++ lua/SkidCheck/sv_SkidList_A.lua
@@ -25,6 +25,7 @@ local Skiddies 	= {
 
 
 
+	[SK.."0:127400248"] = "Reaper/imamoon/Troll names, Smeghack",
 	[SK.."0:83661830"] = "NitroZ/Nitroz59/Mine59, BlueBot aimbot",
 	[SK.."0:170827760"] = "bj_boy/DerBoy/Sprachchat/Troll names, AHack, VAC banned, Ban Evasion (Alt of STEAM_0:1:58820780)",
 	[SK.."1:58820780"] = "bj_boy/DerBoy/Sprachchat/Troll names, AHack, VAC banned, Ban Evasion (STEAM_0:0:170827760)",
